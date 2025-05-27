65d4471e7aa2da67b0b879725c9906a3f2dabcf2
miao0044 <60822240+miao0044@users.noreply.github.com>
2025-05-26 19:46:18 -0500

    Add files via upload

---
+++ config_en.py
@@ -0,0 +1,14 @@
+# config.py
+# Reddit account credentials for multiple accounts
+ACCOUNTS = [
+    {
+        "username": "reddit-user",
+        "password": "xxxxx",
+        "client_id": "xxxxx",
+        "client_secret": "xxxxx",
+        "user_agent": "1"
+    },
+]
+
+# Target users to reply to
+TARGET_USERS = ["reddit-user"]
